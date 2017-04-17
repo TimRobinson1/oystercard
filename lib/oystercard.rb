9a1dc8c613adfa0f3196866937d49d074e24ff89
Timothy Robinson <timmyrobby@gmail.com>
2017-04-17 20:58:53 +0100

    Completing step 6

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,20 @@
 class Oystercard
   attr_reader :balance
 
+  BALANCE_LIMIT = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    raise "Limit of £#{BALANCE_LIMIT} reached" if limit_reached?(amount)
     @balance += amount
+  end
+
+  private
+
+  def limit_reached?(amount)
+    @balance + amount > BALANCE_LIMIT
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,5 +15,20 @@ describe Oystercard do
       card.top_up(10)
       expect(card.balance).to eq 10
     end
+
+    describe 'raises error' do
+
+      it 'when increased over limit' do
+        message = "Limit of £#{Oystercard::BALANCE_LIMIT} reached"
+        expect { card.top_up(91) }.to raise_error message
+      end
+
+      it 'when added incrementally over limit' do
+        message = "Limit of £#{Oystercard::BALANCE_LIMIT} reached"
+        card.top_up(40)
+        card.top_up(45)
+        expect { card.top_up(10) }.to raise_error message
+      end
+    end
   end
 end
