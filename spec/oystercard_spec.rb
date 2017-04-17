5a48b38a556f3b3089490a72529006a8c491cbac
Timothy Robinson <timmyrobby@gmail.com>
2017-04-17 19:34:03 +0100

    Completing step 5

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,7 +3,17 @@ require 'oystercard'
 describe Oystercard do
   subject(:card) { described_class.new }
 
+  it { is_expected.to respond_to :balance }
+  it { is_expected.to respond_to :top_up }
+
   it 'has a starting balance of 0' do
     expect(card.balance).to eq 0
+  end
+
+  describe '#top_up' do
+    it 'can add to the balance' do
+      card.top_up(10)
+      expect(card.balance).to eq 10
+    end
   end
 end
